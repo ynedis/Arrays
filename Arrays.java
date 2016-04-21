4547625da47fff757e2b9ba62651e00bff288dd8
Marta Pardo <ynedisanima@gmail.com>
2016-04-21 12:45:30 +0200

    Impresión pos0 de un array de Ints

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,7 @@
 public class Arrays{
   public static void main(String[] args){
+    int[] arrayInt = {12,34,54,23,54};
     System.out.println("Vamos a empezar con array");
+    System.out.println(arrayInt[0]);
   }
 }
