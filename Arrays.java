2545b313ca7a799d151c8c70064935d1a5807085
Marta Pardo <ynedisanima@gmail.com>
2016-04-21 12:53:04 +0200

    Nuevo array de 20 e impresión pos 0

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,8 @@
 public class Arrays{
   public static void main(String[] args){
+    int[] intArray = new int[20];
+    intArray[0]=5453;
     System.out.println("Vamos a empezar con array");
+    System.out.println("Pos 0 de array 20 " + intArray[0]);
   }
 }
